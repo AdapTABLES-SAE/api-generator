539bfb438eabd29f061d9ba15d093f3c4c093785
Bérénice <berenice.lemoine@univ-lemans.fr>
2023-01-06 18:23:27 +0100

    add missing elements from previous push

--- Generator/tests/edu_elements_tests/ObjectiveSelectionTest.java
+++ Generator/tests/edu_elements_tests/ObjectiveSelectionTest.java
@@ -1,4 +1,4 @@
-package objectif_tests;
+package edu_elements_tests;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
--- Generator/tests/edu_elements_tests/TasksSelectionTest.java
+++ Generator/tests/edu_elements_tests/TasksSelectionTest.java
@@ -1,4 +1,4 @@
-package objectif_tests;
+package edu_elements_tests;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
