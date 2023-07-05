efc3283f0d6a8826268e0ca1e6de7045651ef495
Bérénice <berenice.lemoine@univ-lemans.fr>
2023-07-05 14:00:11 +0200

    update and bug correction of API

---
+++ FrameworkAPI/src/main/java/api_code/LearnerPlayerManagerResource.java
@@ -0,0 +1,117 @@
+package api_code;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import generator.CurrentObjectiveLevel;
+import generator.QuestionableFact;
+import generator.QuestionableFactResult;
+import generator.ResultsByTask;
+import generator.impl.QuestionableFactResultImpl;
+import jakarta.servlet.ServletContext;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import managers.ModelsManager;
+
+@Path("/learnerPlayerManager")
+public class LearnerPlayerManagerResource {
+
+	private ModelsManager modelsManager;
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String addingLearnerPlayerResults(String jsonContent, @Context ServletContext app) { 
+		Paths.PROJECT_PATH = app.getRealPath("");
+		JSONObject obj = new JSONObject();
+		try {
+			obj = (JSONObject) new JSONParser().parse(jsonContent);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		modelsManager = new ModelsManager(Paths.PROJECT_PATH + Paths.INPUT_MODELS_PATH, 
+				Paths.PROJECT_PATH + Paths.OUTPUT_MODELS_PATH, 
+				Paths.CONTEXTS_FILES_PATH + Paths.CONTEXTS_FILES_SHORT_NAME + obj.get("learnerID") + ".xmi", true);
+				
+		CurrentObjectiveLevel col = getCorrespondingCOL((String) obj.get("objectiveID"), (String) obj.get("levelID"));
+		if(col != null) {
+			JSONArray results = (JSONArray) obj.get("resultsByTasks");
+			for (Object object : results) {
+				JSONObject taskResult = (JSONObject) object;
+				ResultsByTask rbt = getCorrespondingResultsByTask((String) taskResult.get("taskID"), col);
+				if(rbt != null) {
+					addResultsToTask(taskResult, rbt);
+				} else {
+					System.err.println("ResultsByTask not found with ID = " + (String) taskResult.get("taskID"));
+				}
+			}
+		} else {
+			System.err.println("CurrentObjectiveLevel not found for O/L = (" + (String) obj.get("objectiveID") + " , " + (String) obj.get("levelID") + ")");
+		}
+		
+		modelsManager.saveContextModel();
+		return "TOTO";
+	}
+
+	/***********************************/
+	/**          JOB METHODS          **/
+	/***********************************/
+	
+	private void addResultsToTask(JSONObject jtask, ResultsByTask rbt) {
+		JSONArray facts = (JSONArray) jtask.get("questionableFacts");
+		for (Object object : facts) {
+			JSONObject factResult = (JSONObject) object;
+			QuestionableFact qf = getCorrespondingQuestionnableFact((String) factResult.get("questionableFactID"), rbt);
+			if(qf != null) {
+				QuestionableFactResult qfres = new QuestionableFactResultImpl();
+				qfres.setAnswerValid((boolean) factResult.get("isCorrect"));
+				qfres.setResponseTime((int) (long) factResult.get("responseTime"));
+				JSONArray answers = (JSONArray) factResult.get("answers");
+				for (Object answer : answers) {
+					qfres.getGivenAnswers().add(answer+"");
+				}
+				qf.getResults().add(qfres);
+			} else {
+				System.err.println("Questionnable fact not found with ID = " + (String) factResult.get("questionableFactID"));
+			}
+		}
+	}
+	
+	private QuestionableFact getCorrespondingQuestionnableFact(String factID, ResultsByTask rbt) {
+		for (QuestionableFact qf : rbt.getQuestionableFacts()) {
+			if(qf.getID().equals(factID)) {
+				return qf;
+			}
+		}
+		return null; 
+	}
+	
+	private ResultsByTask getCorrespondingResultsByTask(String taskID, CurrentObjectiveLevel col) {
+		for (ResultsByTask rbt : col.getResults().getResultsbytask()) {
+			if(rbt.getTask().getID().equals(taskID)) {
+				return rbt;
+			}
+		}
+		return null;
+	}
+	
+	private CurrentObjectiveLevel getCorrespondingCOL(String objectiveID, String levelID) {
+		for (CurrentObjectiveLevel col : modelsManager.getContextModel().getLearnerplayer().getProgression().getCurrentobjectivelevels()) {
+			System.out.println(col.getObjective().getID()+" "+col.getLevel().getID());
+			System.out.println(col.getObjective().getID().equals(objectiveID));
+			System.out.println(col.getLevel().getID().equals(levelID));
+			if(col.getObjective().getID().equals(objectiveID) && col.getLevel().getID().equals(levelID)) {
+				return col;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ FrameworkAPI/src/main/java/api_code/Paths.java
@@ -0,0 +1,15 @@
+package api_code;
+
+
+public class Paths {
+		
+	public static String PROJECT_PATH; 
+	public static final String ECORE_PATH = "models/ecore/";
+	public static final String FLATNER_PATH = "flattener/";
+	public static final String INPUT_MODELS_PATH = "models/inputmodels/";
+	public static final String OUTPUT_MODELS_PATH = "models/outputmodels/";
+	public static final String CONTEXTS_FILES_SHORT_NAME = "Context_";
+	
+	public static final String CONTEXTS_FILES_PATH = "contextsByLP/"; 
+	
+}
