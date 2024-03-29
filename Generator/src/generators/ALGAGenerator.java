ebcb3defd9d7ea96c65523eb7d1aa5b04133c144
Bérénice <berenice.lemoine@univ-lemans.fr>
2024-03-29 11:21:15 +0100

    gestion libs

--- Generator/src/generators/ALGAGenerator.java
+++ Generator/src/generators/ALGAGenerator.java
@@ -36,7 +36,7 @@ public class ALGAGenerator {
 	private Dungeon generatedDungeon;
 	private DungeonElements dungeonElements;
 	
-	public static DidacticDomain DOMAIN = DidacticDomain.SOLAR_SYSTEM;
+	public static DidacticDomain DOMAIN = DidacticDomain.MATHEMATICS;
 	
 	public static boolean MAXIMIZE_ROOMTYPE_ACCESS = false;
 	
@@ -44,7 +44,7 @@ public class ALGAGenerator {
 		for(int i = 0; i < 1; i++) {
 			ALGAGenerator generator;
 			try {
-				generator = new ALGAGenerator("SOLAR");
+				generator = new ALGAGenerator("FICTIF01");
 				generator.generate();
 				generator.printDungeon();
 				generator.saveDungeon("DungeonGen.xmi");
